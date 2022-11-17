de8f58611f7e51a7cdf8698d434ac768371f4bb7
kaka <vn503024@gmail.com>
2022-11-17 15:01:09 +0800

    chore(C++): added the template of C++

---
+++ C++/template/clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# cmake
+rm -rf build
---
+++ C++/template/src/main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string A = "Hello World";
+  cout << A << endl;
+  return 0;
+}
